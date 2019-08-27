99256670aac449fd56db1c9ff633499e384b3529
ishant <ishantgupta777@gmail.com>
2019-08-28 04:41:50 +0530

    initital comit with latitude

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {lat : null, err : ''}
+        window.navigator.geolocation.getCurrentPosition(
+            pos => this.setState({lat : pos.coords.latitude}),
+            err => this.setState({err : err.message})
+        )
+    }
+    
+    render(){
+       if(this.state.lat){
+        return(
+            <div>
+                latitude : {this.state.lat}
+            </div>
+        )
+       }else if(this.state.err){
+           return(
+               <div>
+                   err : {this.state.err}
+               </div>
+           )
+       }else{
+           return <div> Loading... </div>
+       }
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App/>,document.querySelector('#root'))
